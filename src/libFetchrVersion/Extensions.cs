8a13b97bc172d71ea58729479f5c7cc296d350b0
Dave Pusey <dave@puseyuk.co.uk>
2024-04-23 22:38:35 +0100

    Add a ToVersionString() extension method

---
+++ src/libFetchrVersion/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace libFetchrVersion
+{
+	public static class Extensions
+	{
+		public static string ToVersionString(this FetchrVersion version)
+		{
+			return version switch
+			{
+				FetchrVersion.Fetchr50 => "5.0",
+				FetchrVersion.Fetchr501 => "5.0.1",
+				FetchrVersion.Fetchr51 => "5.1",
+				FetchrVersion.Fetchr511 => "5.1.1",
+				FetchrVersion.Fetchr512 => "5.1.2",
+				_ => throw new NotImplementedException(),
+			};
+		}
+	}
+}
